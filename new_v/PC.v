b56d4390b2df387fb5a417fb4d3da7872ed38bfa
pietros <pietro.levo@outlook.com>
2025-11-18 22:52:07 +0100

    feat: first version of the program counter
    Signed-off-by: pietros <pietro.levo@outlook.com>

---
+++ new_v/PC.v
@@ -0,0 +1,21 @@
+///////////////////////////////////////////////////////////////////////////
+// Engineer: Pietro Alberto Levo
+// File: PC
+// Description: implementation of a simple Program Counter 
+///////////////////////////////////////////////////////////////////////////
+
+module PC (clk, rst, pc_in, pc_out);
+	input 	clk;
+	input		rst;
+	input		[31:0] pc_in;
+	output	reg [31:0] pc_out;
+
+	always @(posedge clk)
+	begin
+		if (rst) begin
+			pc_out <= 32'b0;
+		end else begin
+			pc_out <= pc_in;
+		end
+	end
+endmodule
